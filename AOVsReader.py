6541a08899d8b294dfb6a667926b54955aeaad61
GastaP765 <gasta.psychic.bird@gmail.com>
2018-10-29 00:53:54 +0900

    初期状態に対応できるように

--- AOVsReader.py
+++ AOVsReader.py
@@ -24,13 +24,22 @@ def AOVsRead():
         for i in range(cnt):
             if caod[i] != 'defaultArnoldDriver':
                 mc.delete(caod[i])
+
+    caro = mc.ls(typ='aiOptions')
+    if 'defaultArnoldRenderOptions' not in caro:
+        mc.createNode('aiOptions', n='defaultArnoldRenderOptions')
+        mc.createNode('aiAOVDriver', n='defaultArnoldDisplayDriver')
+        mc.connectAttr('defaultArnoldDisplayDriver.message', 'defaultArnoldRenderOptions.drivers[0]')
+    if 'defaultArnoldFilter' not in caof:
+        mc.createNode('aiAOVFilter', n='defaultArnoldFilter')
+    if 'defaultArnoldDriver' not in caod:
+        mc.createNode('aiAOVDriver', n='defaultArnoldDriver')
     
     logging.debug('AOVsRead')
     get = open(path)
     a = json.load(get)
     b = a['arnold']
 
-    # AOVs
     for z in b.keys():
         if z == 'outputs':
             y = b[z]
