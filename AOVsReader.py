d12914a95203eeea63c69c78e54e39f0052b04ee
patriot6991 <patriot6991@gmail.com>
2018-10-29 01:52:11 +0900

    Merge remote-tracking branch 'remotes/origin/master' into tmp2
    # Conflicts:
    #	AOVsReader.py

--- AOVsReader.py
+++ AOVsReader.py
@@ -4,6 +4,7 @@ import logging
 
 path = r'\\172.29.44.4\cg\ms06\msJobPitcher\AOVs\aov.json'
 
+
 def AOVsRead():
     caov = mc.ls(typ='aiAOV')
     cnt = len(caov)
@@ -24,6 +25,16 @@ def AOVsRead():
         for i in range(cnt):
             if caod[i] != 'defaultArnoldDriver':
                 mc.delete(caod[i])
+
+    caro = mc.ls(typ='aiOptions')
+    if 'defaultArnoldRenderOptions' not in caro:
+        mc.createNode('aiOptions', n='defaultArnoldRenderOptions')
+        mc.createNode('aiAOVDriver', n='defaultArnoldDisplayDriver')
+        mc.connectAttr('defaultArnoldDisplayDriver.message', 'defaultArnoldRenderOptions.drivers[0]')
+    if 'defaultArnoldFilter' not in caof:
+        mc.createNode('aiAOVFilter', n='defaultArnoldFilter')
+    if 'defaultArnoldDriver' not in caod:
+        mc.createNode('aiAOVDriver', n='defaultArnoldDriver')
 
     logging.debug('AOVsRead')
     get = open(path)
