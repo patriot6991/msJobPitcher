c2bbaf101e2775d3ce07d6d6cee638d5ac25af7c
patriot6991 <patriot6991@gmail.com>
2018-10-30 20:03:17 +0900

    拡張子の表示順変更 各ブラウズボタンに機能割り当て

--- browsFile.py
+++ browsFile.py
@@ -2,9 +2,9 @@ import logging
 import maya.cmds as mc
 
 
-basicFiles = "MayaFiles(*.ma *.mb);;Maya Ascii(*.ma);;Maya Binary(*.mb);;OBJ(*.obj);;FBX(*.fbx);;All Files(*.*)"
+basicFiles = "All Files(*.*);;MayaFiles(*.ma *.mb);;Maya Ascii(*.ma);;Maya Binary(*.mb);;OBJ(*.obj);;FBX(*.fbx)"
 
 def search(defPath=''):
     selectFiles = mc.fileDialog2(ff=basicFiles,fm=1,ds=1,dir=defPath,caption="selectFiles")
-    selectFiles = r"{}".format(selectFiles)
+    selectFiles = r"{}".format(selectFiles[0])
     return selectFiles
